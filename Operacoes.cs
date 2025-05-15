b568ac7028bb469e45752da2f8ea010c79885e5f
AlexSSnts <88456345+AlexSSnts@users.noreply.github.com>
2025-05-15 10:48:37 -0300

    Operacoes.cs

---
+++ Operacoes.cs
@@ -0,0 +1,29 @@
+
+using System;
+
+public class Operacoes
+{
+	public Operacoes()
+	{
+	}
+	public double Soma(double a, double b)
+		{
+			return a + b;
+		}
+	public double Subtracao(double a, double b)
+	{
+		return a - b;
+	}
+	public double Multiplicacao(double a, double b)
+	{
+		return a * b;
+	}
+	public double Dvisao(double a, double b)
+	{
+		return a / b;
+	}
+	public double Porcetangem(double a, double b)
+	{
+		return a * (b / 100);
+	}
+}
